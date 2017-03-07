f4b4770e7ecdd522293870b95dba44ebcac02ccf
Paulo Matos <paulo.matos@itriad.org.br>
2017-03-07 12:10:19 -0400

    Limit pipes position

--- Assets/Scripts/PipesController.cs
+++ Assets/Scripts/PipesController.cs
@@ -10,18 +10,30 @@ public class PipesController : MonoBehaviour {
 	// rigid body
 	private Rigidbody2D rigidBody2d;
 
+	// pipes Y limits
+	float maxY = 5f;
+	float minY = -6.3f;
+
 	// Use this for initialization
 	void Start () {
 		rigidBody2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {		
+		CheckPipesPosition ();
 		MovePipes ();
 	}
 
+	void CheckPipesPosition() {
+		if (transform.position.y < minY) {			
+			rigidBody2d.constraints = RigidbodyConstraints2D.FreezePositionY;
+			Debug.Log ("Game Over!!!");
+		}
+	}
+
 	void MovePipes() {
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space) && transform.position.y < maxY) {
 			FlappyPipes ();
 		}
 	}
