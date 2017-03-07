1c56cb2ab4c545dc1c4c0d89b3bbe561c9f4ec5a
Paulo Matos <paulo.matos@itriad.org.br>
2017-03-07 11:03:15 -0400

    Initial pipes implemented

---
+++ Assets/Scripts/PipesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesController : MonoBehaviour {
+
+	[Header("Velocity")]
+	public float pipesVelocity;
+
+	// rigid body
+	private Rigidbody2D rigidBody2d;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MovePipes ();
+	}
+
+	void MovePipes() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			FlappyPipes ();
+		}
+	}
+
+	void FlappyPipes() {
+		rigidBody2d.velocity = Vector2.up * pipesVelocity;
+	}
+
+}
